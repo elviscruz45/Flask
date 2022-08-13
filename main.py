dcb7d8d3fafeac87a6c1812aa523f5340a523155
elviscruz45 <teseosoftwarecompany@gmail.com>
2022-08-13 16:36:16 -0500

    Bootstrap_WFT

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+#bootstrap
 from ensurepip import bootstrap
-from flask import Flask,request,make_response,redirect, render_template
+#Flask
+from flask import Flask,request,make_response,redirect, render_template,session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
 
+#wtf formularios
+from wtforms.fields import StringField, PasswordField,SubmitField
+from wtforms.validators import DataRequired
+
+
+#iniciar la aplicacion Flask
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
+app.config["SECRET_KEY"]="SUPER SECRETICIMO"
+
 todos=["Comprar cafe","Enviar solicitud de compra","Entregar video a productor"]
 
+#Crear formularios
+
+class LoginForm(FlaskForm):
+    username=StringField("Nombre de usuario",validators=[DataRequired()])
+    password=PasswordField("Password",validators=[DataRequired()])
+    submit=SubmitField("Enviar")
 
 # error managements
 
@@ -24,19 +41,26 @@ def server_error(error):
 def index():
     user_ip=request.remote_addr
     response=make_response(redirect("/hello"))
-    response.set_cookie("user_ip",user_ip)
+    #response.set_cookie("user_ip",user_ip)
+    session["user_ip"]=user_ip
+    
     return response
 
 
 
-@app.route("/hello")
+@app.route("/hello")    #,methods=["GET","POST"])
 def hello():
-    user_ip=request.cookies.get("user_ip")
+    #user_ip=request.cookies.get("user_ip")
+    user_ip=session.get("user_ip")
+    login_form=LoginForm()
     context={
         "user_ip":user_ip,
         "todos":todos,
+        "login_form":login_form
     }
     return render_template("hello.html",**context)
+
+
 
 
 if __name__=="__main__":
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 source env/bin/activate
 export FLASK_APP=main.py
 export FLASK_DEBUG=1
+export FLASK_ENV=development
 
 flask run

e1830463eff0e4f4866cff2d3ca0b64acb1b7c7e
elviscruz45 <teseosoftwarecompany@gmail.com>
2022-08-13 17:18:11 -0500

    POST_WTF

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #bootstrap
 from ensurepip import bootstrap
 #Flask
-from flask import Flask,request,make_response,redirect, render_template,session
+from flask import Flask,request,make_response,redirect, render_template,session , url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 
@@ -48,17 +48,29 @@ def index():
 
 
 
-@app.route("/hello")    #,methods=["GET","POST"])
+@app.route("/hello",methods=["GET","POST"])
 def hello():
     #user_ip=request.cookies.get("user_ip")
     user_ip=session.get("user_ip")
     login_form=LoginForm()
+    username=session.get("username")
     context={
         "user_ip":user_ip,
         "todos":todos,
-        "login_form":login_form
+        "login_form":login_form,
+        "username":username
     }
+    
+    if login_form.validate_on_submit():
+        username=login_form.username.data
+        session["username"]=username
+        return redirect(url_for("index"))
+
     return render_template("hello.html",**context)
+
+
+
+
 
 
 
